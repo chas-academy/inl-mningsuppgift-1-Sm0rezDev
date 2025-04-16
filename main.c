bc589c066097519c518bb869477d48916365105f
Sm0rezDev <simonw0210@gmail.com>
2025-04-16 22:30:53 +0200

    Looks great, got the expected vlaues, next is to calculate the sum of all the vlaue and the mean value

--- main.c
+++ main.c
@@ -17,5 +17,21 @@ int main()
         dice_rolls[i] = dice;
     }
 
+    int rolls[8] = {0}; // initialize array with zero.
+
+    for (int i = 0; i < MAX_ROLLS; ++i) {
+        int dice_value = dice_rolls[i];
+        (dice_rolls[i] == 1) ? rolls[dice_value - 1]++ : (void)0;
+        (dice_rolls[i] == 2) ? rolls[dice_value - 1]++ : (void)0;
+        (dice_rolls[i] == 3) ? rolls[dice_value - 1]++ : (void)0;
+        (dice_rolls[i] == 4) ? rolls[dice_value - 1]++ : (void)0;
+        (dice_rolls[i] == 5) ? rolls[dice_value - 1]++ : (void)0;
+        (dice_rolls[i] == 6) ? rolls[dice_value - 1]++ : (void)0;
+    }
+
+    for (int i = 0; i < (sizeof(rolls) / sizeof(int)); ++i) {
+        printf("%d\n", rolls[i]);
+    }
+
     return 0;
 }

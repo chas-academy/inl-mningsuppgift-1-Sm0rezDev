4c879768c0508b4f5e9e89e6a1658a6768f19845
Sm0rezDev <simonw0210@gmail.com>
2025-04-16 20:50:13 +0200

    Dice roll works

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_ROLLS 100
+
 int main()
 {
     // The Line below must *NOT* be changed or test will fail!
@@ -8,6 +10,12 @@ int main()
 
    
     // Your code goes here
+    unsigned int dice_rolls[MAX_ROLLS];
+
+    for (int i = 0; i < MAX_ROLLS; ++i) {
+        int dice = (rand()%6) + 1;
+        printf("%d\n", dice);
+    }
 
     return 0;
 }
